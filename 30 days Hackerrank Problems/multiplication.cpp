5c5795cd257ddf1aa5d62f54b52ebfcc6b4570a8
mondol2023 <abm20154@gmail.com>
2023-09-19 22:28:00 +0600

    first hackerrank problem

---
+++ 30 days Hackerrank Problems/multiplication.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "enter 1st num: ";
+    int 1stnum = 0;
+    cin >> 1stnum;
+    cout << "enter 2nd num: ";
+    int 2ndnum =0;
+    cin >> 2ndnum;
+
+    int multiplication = 1stnum*2ndnum;
+
+    cout << 1stnum<<"x"<< 2ndnum;
+    cout << "=" << multiplication << endl;
+
+    return 0;
+}
